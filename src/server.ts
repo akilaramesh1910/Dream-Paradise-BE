eca8499f572fcabaab5934d5bc756e0d8312852c
akilaramesh1910 <akilaramesh1910@gmail.com>
2025-11-23 09:12:40 +0530

    a

--- src/server.ts
+++ src/server.ts
@@ -28,7 +28,11 @@ const app = express();
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+
+app.use(express.json({ limit: "10mb" }));
+
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
 app.use(morgan('dev'));
 
 // API Routes
